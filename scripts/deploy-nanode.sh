97de8860ef1e702dc6910e456f543365286ad342
GPerks <4460511+git-garrett@users.noreply.github.com>
2026-07-07 11:23:05 -0700

    Enable Rust primary by default in production deploy

--- scripts/deploy-nanode.sh
+++ scripts/deploy-nanode.sh
@@ -78,7 +78,7 @@ Environment=HOST=${REMOTE_BIND_HOST}
 Environment=PORT=${REMOTE_PORT_APP}
 Environment=CRIBBAGE_STATIC_DIR=${REMOTE_APP_DIR}/dist
 Environment=CRIBBAGE_DB_PATH=${REMOTE_DATA_DIR}/cribbage-server.sqlite
-Environment=CRIBBAGE_RUST_PRIMARY=${CRIBBAGE_RUST_PRIMARY:-0}
+Environment=CRIBBAGE_RUST_PRIMARY=${CRIBBAGE_RUST_PRIMARY:-1}
 Environment=CRIBBAGE_RUST_PRIMARY_MODELS=${CRIBBAGE_RUST_PRIMARY_MODELS:-schell_table-peg_table-13.0,schell_table-peg_table-14.3,schell_table-peg_table-15.0,schell_table-peg_table-15.1,schell_table-peg_table-15.2}
 Environment=CRIBBAGE_RUST_SHADOW=${CRIBBAGE_RUST_SHADOW:-0}
 Environment=CRIBBAGE_RUST_SHADOW_BIN=${REMOTE_APP_DIR}/rust/cribbage-shadow-engine/cribbage-shadow-engine
